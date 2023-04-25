9dcf41c3e917bdec516807c14de500861763bf62
h4hn <hahnischiltach@gmail.com>
2023-04-25 20:50:11 +0200

    Added Organisation Card and Usermanagement in Organisation.

--- src/app/usermanagement-in-organisation/usermanagement-in-organisation.component.ts
+++ src/app/usermanagement-in-organisation/usermanagement-in-organisation.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {listData} from "./user-list";
 
 @Component({
   selector: 'app-usermanagement-in-organisation',
@@ -7,6 +8,21 @@ import {Component, EventEmitter, Output} from '@angular/core';
 })
 export class UsermanagementInOrganisationComponent {
   @Output() onClose = new EventEmitter<void>();
+
+  userList= listData;
+
+  @ViewChild('searchbar') searchbar: ElementRef;
+  searchText = '';
+
+  filterUsers() {
+    if (!this.searchText) {
+      return this.userList;
+    }
+    return this.userList.filter(user => {
+      return user.userEmail.toLowerCase().includes(this.searchText.toLowerCase());
+    });
+  }
+
   closeCard() {
     this.onClose.emit();
   }
