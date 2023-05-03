23b34a68f02c7f9e07cd86bc97e74cff048098a9
FabUnger <fabian.unger@magenta.de>
2023-05-03 17:14:20 +0200

    Orga Image und Event Image überall eingefügt

--- src/app/organisation-banner-upload/organisation-banner-upload.component.ts
+++ src/app/organisation-banner-upload/organisation-banner-upload.component.ts
@@ -4,6 +4,7 @@ import {FormControl} from "@angular/forms";
 import {UiUserService} from "../../services/ui-user.service";
 import {Organisation} from "../../DataTransferObjects/Organisation";
 import {UiAdminService} from "../../services/ui-admin.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-organisation-banner-upload',
@@ -11,7 +12,7 @@ import {UiAdminService} from "../../services/ui-admin.service";
   styleUrls: ['./organisation-banner-upload.component.css']
 })
 export class OrganisationBannerUploadComponent {
-  constructor(private uiOrganizerService: UiOrganizerService, private uiUserService: UiUserService, private uiAdminService: UiAdminService) {
+  constructor(private uiOrganizerService: UiOrganizerService, private uiUserService: UiUserService, private uiAdminService: UiAdminService, private snackBar: MatSnackBar) {
     this.fileControl = new FormControl(this.file)
   }
 
@@ -20,7 +21,13 @@ export class OrganisationBannerUploadComponent {
 
     this.fileControl.valueChanges.subscribe((file: any) => {
       this.file = file;
-      this.uiAdminService.changeOrganisation(this.orga, this.file).subscribe();
+      if (this.file.size > 1048576) {
+        this.snackBar.open("Image-Größe maximal 1MB", 'Close', { duration: 5000 });
+      } else {
+        this.uiAdminService.changeOrganisation(this.orga, this.file).subscribe();
+        this.closeOrganisationBannerUpload();
+      }
+
     });
 
   if(orgaId != null) {
